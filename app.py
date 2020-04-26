4901a08dc325ad1ce880a694559ab72a12af1503
dxmski <dominic@DESKTOP-5NII6GU.localdomain>
2020-04-26 02:37:47 +0100

    updated css file to show video in background

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask
-from flask import render_template
-from functions import create_table, insert_table, view_table
+from flask import Flask, render_template, g
 import requests
+import sqlite3
+import os
 
-app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "money.db")
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(db_path)
+    return db
+
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cur = get_db().cursor()
+    cur.execute("""SELECT * FROM 'money'; """)
+    items = cur.fetchall()
+    
+    return render_template('index.html', items=items)
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
