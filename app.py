59712a6f1c7fa9d9cbdea8722ad4b73130f63a8b
dxmski <dominicroberts5@hotmail.com>
2020-05-03 18:49:16 +0000

    allowed flask app to be connected too via lan

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/.venv/bin/python
+
 from flask import Flask, render_template, g
 import requests
 import sqlite3
@@ -8,26 +10,29 @@ db_path = os.path.join(BASE_DIR, "money.db")
 
 
 def get_db():
-    db = getattr(g, '_database', None)
+    db = getattr(g, "_database", None)
     if db is None:
         db = g._database = sqlite3.connect(db_path)
     return db
 
-app = Flask(__name__, static_folder='static')
 
-@app.route('/')
+app = Flask(__name__, static_folder="static")
+
+
+@app.route("/")
 def index():
     cur = get_db().cursor()
     cur.execute("""SELECT * FROM 'money'; """)
     items = cur.fetchall()
-    
-    return render_template('index.html', items=items)
+    return render_template("index.html", items=items)
+
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, '_database', None)
+    db = getattr(g, "_database", None)
     if db is not None:
         db.close()
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
